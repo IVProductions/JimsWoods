346abb04433b3d0047d1e77bc61b1bd2ac20603a
simoneik <simonhordvik@Simons-MacBook-Pro.local>
2013-09-08 16:44:33 -0700

    made buttons with style from mainMenu.css in the first.html partial

--- app/controllers/firstCtrl.js
+++ app/controllers/firstCtrl.js
@@ -1,3 +1,6 @@
-function firstCtrl($scope){
-
+function firstCtrl($scope, $location){
+    $scope.checkSlot=function(slotNumber){                 //should check local storage to find out if the slot contains a started game
+        //alert(slotNumber);                               //else it should start a new game
+        $location.path("ass");
+    }
 }
