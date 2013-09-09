68aa03309208a4e906ffb0d18317debb2579200f
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-09-09 23:04:07 +0200

    melonjs

--- app/controllers/gameCtrl.js
+++ app/controllers/gameCtrl.js
@@ -1,46 +1,85 @@
 function gameCtrl($scope, stateService){
-    var game = {
-        // Run on page load.
-        "onload" : function () {
-            // Initialize the video.
-            if (!me.video.init("screen", 640, 480, true, 'auto')) {
-                alert("Your browser does not support HTML5 canvas.");
-                //return;
-            }
-            else{
-                alert("Your browser support HTML5 canvas.");
-            }
+    var game = function () {
+        // Init Canvas
+        me.video.init("screen", 960, 640, true);
 
-            /* add "#debug" to the URL to enable the debug Panel
-            if (document.location.hash === "#debug") {
-                window.onReady(function () {
-                    me.plugin.register.defer(debugPanel, "debug");
-                });
-            }
+        // Initialize the audio.
+        //me.audio.init("mp3,ogg");
 
-            // Initialize the audio.
-            me.audio.init("mp3,ogg");
+        // Set a callback to run when loading is complete.
+        //me.loader.onload = this.loaded.bind(this);
+        // Set a callback to run when loading is complete.
+        // Load the resources.
+        me.loader.preload(game.resources);
+        // Initialize melonJS and display a loading screen.
+        //me.state.change(me.state.LOADING);
+        console.log("preloaddone");
+        me.state.change(me.state.LOADING);
 
-            // Set a callback to run when loading is complete.
-            me.loader.onload = this.loaded.bind(this);
+        loaded();
 
-            // Load the resources.
-            me.loader.preload(game.resources);
+    };
 
-            // Initialize melonJS and display a loading screen.
-            me.state.change(me.state.LOADING);
+    var loaded = function(){
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // Start the game.
+        me.state.change(me.state.PLAY);
+    }
+
+    game.resources = [
+    /**
+     * Graphics.
+     */
+        // our level tileset
+        {
+            name: "area01_level_tiles",  type:"image", src: "Content/tiles/area01_level_tiles.png"
+        },
+
+        /*
+         * Maps.
+         */
+        {
+            name: "area01", type: "tmx", src: "Content/map/area01.tmx"
+        }
+
+    ];
+
+    game.PlayScreen = me.ScreenObject.extend({
+
+        onResetEvent: function() {
+            // stuff to reset on state change
+            // load a level
+            me.levelDirector.loadLevel("area01");
+        },
+
+        /* ---
+
+         action to perform when game is finished (state change)
+
+         --- */
+        onDestroyEvent: function() {
+        }
+
+    });
+
+    game.TitleScreen = me.ScreenObject.extend({
+        /**
+         *  action to perform on state change
+         */
+        onResetEvent: function() {
         },
 
 
+        /**
+         *  action to perform when leaving this screen (state change)
+         */
+        onDestroyEvent: function() {
 
-        // Run on game resources loaded.
-        "loaded" : function () {
-            me.state.set(me.state.MENU, new game.TitleScreen());
-            me.state.set(me.state.PLAY, new game.PlayScreen());
-
-            // Start the game.
-            me.state.change(me.state.PLAY);
-        */
         }
-    };
+    });
+
+
+    game();
 }
