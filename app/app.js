b910ab5d6b6d78ef7c1cd47516a5fe6267705b43
simoneik <simonhordvik@Simons-MacBook-Pro.local>
2013-09-08 18:53:28 -0700

    Created simple menues and json-files to contain animals

--- app/app.js
+++ app/app.js
@@ -4,12 +4,18 @@ var JimsWoods = angular.module('JimsWoods', []);
 <!--Routing-->
 JimsWoods.config(function ($routeProvider){
     $routeProvider.when("/", {
-        templateUrl:"app/partials/first.html",
+        templateUrl:"app/partials/slot.html",
         controller:"firstCtrl"
-    }).when("/ass",{
-            templateUrl:"app/partials/second.html",
+    }).when("/main",{
+            templateUrl:"app/partials/mainMenu.html",
             controller:"firstCtrl"
-        }).otherwise({
+    }).when("/wiki",{
+            templateUrl:"app/partials/wiki.html",
+            controller:"firstCtrl"
+    }).when("/animalList",{
+            templateUrl:"app/partials/animalList.html",
+            controller:"firstCtrl"
+    }).otherwise({
         redirectTo:"/"
     })
 });
--- app/controllers/firstCtrl.js
+++ app/controllers/firstCtrl.js
@@ -1,6 +1,36 @@
-function firstCtrl($scope, $location){
-    $scope.checkSlot=function(slotNumber){                 //should check local storage to find out if the slot contains a started game
-        //alert(slotNumber);                               //else it should start a new game
-        $location.path("ass");
+function firstCtrl($scope, $location, $http){
+    $scope.checkSlot=function(slotNumber){                   //this function should check whether the slot is empty or
+        $location.path("main");                              //has a game saved to it, before it changes to the correct partial
     }
+    $scope.changeView=function(view){
+        $location.path(view);
+    }
+    $scope.chooseWorldList=function(world){
+        //if world = wood then $http.get-> woodAnimals.json
+        var animalFile = "";
+        if (world=="woods") {
+            animalFile="woodsAnimals.json";
+        }
+        else if (world="desert") {
+            animalFile="desertAnimals.json";
+        }
+        else if (world="sea") {
+            animalFile="seaAnimals.json";
+        }
+        else if (world="jungle") {
+            animalFile="jungleAnimals.json";
+        }
+        else if (world="arctic") {
+            animalFile="arcticAnimals.json";
+        }
+        else if (world="fable") {
+            animalFile="fableAnimals.json";
+        }
+        $http.get('app/animals/'+animalFile).success(function(data) {  //get animal objects from json-file
+            $scope.animals = data;
+        });
+        $location.path("animalList");
+    }
+
+    //setter at drop down menyens default er newest phone basert på alder
 }
