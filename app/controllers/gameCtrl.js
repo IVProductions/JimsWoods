48478f8e6230d1910f2a2fb89eb8d660b221dfb7
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-09-10 01:44:12 +0200

    lots of changes for melonjs

--- app/controllers/gameCtrl.js
+++ app/controllers/gameCtrl.js
@@ -1,85 +1,144 @@
 function gameCtrl($scope, stateService){
-    var game = function () {
-        // Init Canvas
-        me.video.init("screen", 960, 640, true);
+    $scope.game = {
+        init : function(){
+            // Create Canvas
+            me.video.init("screen", 960, 640);
 
-        // Initialize the audio.
-        //me.audio.init("mp3,ogg");
+            // Initialize the audio.
+            //me.audio.init("ogg");
 
-        // Set a callback to run when loading is complete.
-        //me.loader.onload = this.loaded.bind(this);
-        // Set a callback to run when loading is complete.
-        // Load the resources.
-        me.loader.preload(game.resources);
-        // Initialize melonJS and display a loading screen.
-        //me.state.change(me.state.LOADING);
-        console.log("preloaddone");
-        me.state.change(me.state.LOADING);
+            // Key bindings.
+            me.input.bindKey(me.input.KEY.UP,       "up");
+            me.input.bindKey(me.input.KEY.LEFT,     "left");
+            me.input.bindKey(me.input.KEY.DOWN,     "down");
+            me.input.bindKey(me.input.KEY.RIGHT,    "right");
 
-        loaded();
+            // Set a callback to run when loading is complete.
+            me.loader.onload = this.loaded.bind(this);
+            this.loadResources();
 
+            // Initialize melonJS and display a loading screen.
+            me.state.change(me.state.LOADING);
+
+        },
+
+        loadResources : function () {
+            // Set all resources to be loaded.
+            var resources = [];
+
+            // Graphics.
+            this.resources["img"].forEach(function forEach(value) {
+                resources.push({
+                    "name"  : value,
+                    "type"  : "image",
+                    "src"   : "Content/img/" + value + ".png"
+                })
+            });
+
+            // Maps.
+            this.resources["map"].forEach(function forEach(value) {
+                resources.push({
+                    "name"  : value,
+                    "type"  : "tmx",
+                    "src"   : "Content/map/" + value + ".tmx"
+                })
+            });
+
+            // Load the resources.
+            me.loader.preload(resources);
+        },
+        "loaded" : function loaded() {
+
+        }
     };
 
-    var loaded = function(){
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+    $scope.game.resources = {
+        "img" : [
+            // UI
+            "dialog",
+            "heart_empty",
+            "heart_half",
+            "heart_full",
+            "logo",
 
-        // Start the game.
-        me.state.change(me.state.PLAY);
+            // Characters
+            "rachel",
+            "rachel_eyes",
+            "abi",
+            "abi_eyes",
+            "george",
+            "george_eyes",
+            "jessica",
+            "jessica_eyes",
+            "whitey",
+            "whitey_eyes",
+            "char_shadow",
+
+            // Baddies
+            "snake",
+
+            // Collectibles
+            "coin_gold",
+            "coin_silver",
+            "coin_shadow",
+            "hammer",
+
+            // Interactive objects
+            "chests",
+
+            // Tile maps
+            /* island */
+            "animwater",
+            "grass",
+            "sandwater",
+            "treetop",
+            "trunk",
+
+            /* rachels_room */
+            "accessories",
+            "cabinets",
+            "country",
+            "floor",
+            "house",
+            "inside",
+            "stairs",
+            "victoria",
+
+            /* rachels_house */
+            "kitchen",
+
+            /* earth */
+            "barrel",
+            "bridges",
+            "buckets",
+            "dirt",
+            "dirt2",
+            "doors",
+            "farming_fishing",
+            "fence",
+            "fence_alt",
+            "flowers_2",
+            "grassalt_flowers",
+            "housey",
+            "misc",
+            "mountains",
+            "shadow",
+            "signs",
+            "stonepattern",
+            "town_buildings",
+            "tree_stump",
+            "victorian_house",
+            "windmill"
+        ],
+
+        /* Maps from Tiled. */
+        "map" : [
+            "island",
+            "earth",
+            "general_store",
+            "rachels_room",
+            "rachels_house",
+            "shed"
+        ]
     }
-
-    game.resources = [
-    /**
-     * Graphics.
-     */
-        // our level tileset
-        {
-            name: "area01_level_tiles",  type:"image", src: "Content/tiles/area01_level_tiles.png"
-        },
-
-        /*
-         * Maps.
-         */
-        {
-            name: "area01", type: "tmx", src: "Content/map/area01.tmx"
-        }
-
-    ];
-
-    game.PlayScreen = me.ScreenObject.extend({
-
-        onResetEvent: function() {
-            // stuff to reset on state change
-            // load a level
-            me.levelDirector.loadLevel("area01");
-        },
-
-        /* ---
-
-         action to perform when game is finished (state change)
-
-         --- */
-        onDestroyEvent: function() {
-        }
-
-    });
-
-    game.TitleScreen = me.ScreenObject.extend({
-        /**
-         *  action to perform on state change
-         */
-        onResetEvent: function() {
-        },
-
-
-        /**
-         *  action to perform when leaving this screen (state change)
-         */
-        onDestroyEvent: function() {
-
-        }
-    });
-
-
-    game();
 }
---
+++ app/game/game.js
@@ -0,0 +1,7 @@
+var game = {
+    "onload" : function onload() {
+        console.log("onload");
+        // Create the canvas
+        me.video.init("screen", 960, 640, true);
+    }
+};
