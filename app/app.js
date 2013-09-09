1549a08f9eb392a69cf0ffe7cd7f9da7bc2b7979
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-09-09 21:56:00 +0200

    melonJS and game.html and gameCtrl

--- app/app.js
+++ app/app.js
@@ -7,23 +7,26 @@ JimsWoods.config(function ($routeProvider){
         templateUrl:"app/partials/slot.html",
         controller:"firstCtrl"
     }).when("/main",{
-            templateUrl:"app/partials/mainMenu.html",
-            controller:"firstCtrl"
+        templateUrl:"app/partials/mainMenu.html",
+        controller:"firstCtrl"
     }).when("/wiki",{
-            templateUrl:"app/partials/wiki.html",
-            controller:"wikiCtrl"
+        templateUrl:"app/partials/wiki.html",
+        controller:"wikiCtrl"
     }).when("/animalList",{
-            templateUrl:"app/partials/animalList.html",
-            controller:"wikiCtrl"
+        templateUrl:"app/partials/animalList.html",
+        controller:"wikiCtrl"
     }).when("/animalDetails",{
-            templateUrl:"app/partials/animalDetails.html",
-            controller:"wikiCtrl"
+        templateUrl:"app/partials/animalDetails.html",
+        controller:"wikiCtrl"
     }).when("/shop",{
-            templateUrl:"app/partials/shop.html",
-            controller:"firstCtrl"
+        templateUrl:"app/partials/shop.html",
+        controller:"firstCtrl"
     }).when("/myAnimals",{
-            templateUrl:"app/partials/myAnimals.html",
-            controller:"firstCtrl"
+        templateUrl:"app/partials/myAnimals.html",
+        controller:"firstCtrl"
+    }).when("/game",{
+        templateUrl:"app/partials/game.html",
+        controller:"gameCtrl"
     }).otherwise({
         redirectTo:"/"
     })
