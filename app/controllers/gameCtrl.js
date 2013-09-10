6608fadd30377523d342bf311a32c188dc0dddcc
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-09-10 03:07:59 +0200

    something is happening

--- app/controllers/gameCtrl.js
+++ app/controllers/gameCtrl.js
@@ -1,4 +1,8 @@
-function gameCtrl($scope, stateService){
+function gameCtrl($scope, stateService, imageResourceFactory, mapResourceFactory){
+    // Get Resource Names
+    $scope.imageResources = imageResourceFactory.images;
+    $scope.mapResources = mapResourceFactory.maps;
+
     $scope.game = {
         init : function(){
             // Create Canvas
@@ -22,39 +26,79 @@ function gameCtrl($scope, stateService){
 
         },
 
-        loadResources : function () {
+        "loadResources" : function () {
             // Set all resources to be loaded.
             var resources = [];
+            var value;
 
             // Graphics.
-            this.resources["img"].forEach(function forEach(value) {
+            for(var i = 0; i<$scope.imageResources.length; i++){
+                value = $scope.imageResources[i].name;
                 resources.push({
-                    "name"  : value,
+                    "name" : value,
                     "type"  : "image",
                     "src"   : "Content/img/" + value + ".png"
                 })
-            });
+            }
 
             // Maps.
-            this.resources["map"].forEach(function forEach(value) {
+            for(var i = 0; i<$scope.mapResources.length; i++){
+                value = $scope.mapResources[i].name;
                 resources.push({
-                    "name"  : value,
+                    "name" : value,
                     "type"  : "tmx",
                     "src"   : "Content/map/" + value + ".tmx"
                 })
-            });
+            }
             // Load the resources.
             me.loader.preload(resources);
         },
-        loaded : function loaded() {
-            // Create a notifier.
-            //game.notify = new game.Notify();
+        "loaded" : function loaded() {
+            me.state.set(me.state.MENU, new game.TitleScreen());
+            me.state.set(me.state.PLAY, new game.PlayScreen());
 
-            // Set the "Play" ScreenObject.
-            game.play = new game.PlayScreen(20);
-            me.state.set(me.state.PLAY, game.play);
+            // Start the game.
+            me.state.change(me.state.PLAY);
         }
     };
+
+    /* the in game stuff*/
+    game.PlayScreen = me.ScreenObject.extend({
+
+        onResetEvent: function() {
+            // stuff to reset on state change
+            // load a level
+            me.levelDirector.loadLevel("island");
+        },
+
+        /* ---
+
+         action to perform when game is finished (state change)
+
+         --- */
+        onDestroyEvent: function() {
+        }
+
+    });
+
+    game.TitleScreen = me.ScreenObject.extend({
+        /**
+         *  action to perform on state change
+         */
+        onResetEvent: function() {
+        },
+
+
+        /**
+         *  action to perform when leaving this screen (state change)
+         */
+        onDestroyEvent: function() {
+        }
+    });
+
+
+
+
 
     $scope.game.resources = {
         "img" : [
