b5f781e9ed4edf7ad4a6ef290ae7c06be25617d5
Gruner16 <kennethjohansen@Kenneth-Gruner-Johansens-MacBook-Pro.local>
2013-09-17 19:59:13 -0700

    animal wiki

--- app/services/jungleAnimalService.js
+++ app/services/jungleAnimalService.js
@@ -1,22 +1,244 @@
     JimsWoods.factory('jungleAnimalFactory',function() {
     var animals = [
         {
-            "id": 1,
-            "name": "Tiger",
+            "id": 77,
+            "name": "Butterfly",
+            "health": 20,
+            "attack": 15,
+            "defence": 10,
+            "speed": 15,
             "description": "blabla",
-            "imageURL": "Content/img/tiger.png"
+            "imageURL": "Content/img/dolphin.png"
         },
         {
-            "id": 2,
-            "name": "Crocodile",
+            "id": 78,
+            "name": "Beetle",
+            "health": 25,
+            "attack": 10,
+            "defence": 50,
+            "speed": 20,
             "description": "blabla",
-            "imageURL": "Content/img/crocodile.png"
+            "imageURL": "Content/img/dolphin.png"
         },
         {
-            "id": 3,
+            "id": 79,
+            "name": "Tapir",
+            "health": 50,
+            "attack": 10,
+            "defence": 40,
+            "speed": 15,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 80,
+            "name": "Toucan",
+            "health": 30,
+            "attack": 35,
+            "defence": 40,
+            "speed": 20,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 81,
+            "name": "Bat",
+            "health": 35,
+            "attack": 35,
+            "defence": 25,
+            "speed": 35,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 82,
+            "name": "Capybara",
+            "health": 30,
+            "attack": 50,
+            "defence": 20,
+            "speed": 30,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 83,
+            "name": "Koala bear",
+            "health": 40,
+            "attack": 35,
+            "defence": 50,
+            "speed": 40,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 84,
+            "name": "Leaf-cutter ant",
+            "health": 40,
+            "attack": 50,
+            "defence": 35,
+            "speed": 40,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 85,
+            "name": "Turtle",
+            "health": 55,
+            "attack": 30,
+            "defence": 70,
+            "speed": 15,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 86,
+            "name": "Centipede",
+            "health": 40,
+            "attack": 45,
+            "defence": 40,
+            "speed": 50,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 87,
+            "name": "Mosquito",
+            "health": 45,
+            "attack": 45,
+            "defence": 40,
+            "speed": 60,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 88,
+            "name": "Capuchin",
+            "health": 55,
+            "attack": 35,
+            "defence": 45,
+            "speed": 70,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 89,
+            "name": "Panda",
+            "health": 80,
+            "attack": 75,
+            "defence": 60,
+            "speed": 35,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 90,
+            "name": "Poison dart frog",
+            "health": 80,
+            "attack": 100,
+            "defence": 55,
+            "speed": 45,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 91,
+            "name": "Tarantula",
+            "health": 70,
+            "attack": 90,
+            "defence": 60,
+            "speed": 60,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 92,
+            "name": "Chimpanzee",
+            "health": 90,
+            "attack": 80,
+            "defence": 65,
+            "speed": 100,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 93,
+            "name": "Anaconda",
+            "health": 120,
+            "attack": 110,
+            "defence": 70,
+            "speed": 40,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 94,
             "name": "Orangutang",
+            "health": 90,
+            "attack": 90,
+            "defence": 90,
+            "speed": 90,
             "description": "blabla",
-            "imageURL": "Content/img/orangutang.png"
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 95,
+            "name": "Jaguar",
+            "health": 80,
+            "attack": 90,
+            "defence": 80,
+            "speed": 110,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 96,
+            "name": "Puma",
+            "health": 70,
+            "attack": 90,
+            "defence": 90,
+            "speed": 110,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 97,
+            "name": "Crocodile",
+            "health": 115,
+            "attack": 115,
+            "defence": 115,
+            "speed": 115,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 98,
+            "name": "Tiger",
+            "health": 105,
+            "attack": 120,
+            "defence": 120,
+            "speed": 120,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 99,
+            "name": "Black mamba",
+            "health": 60,
+            "attack": 180,
+            "defence": 40,
+            "speed": 190,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
+        },
+        {
+            "id": 100,
+            "name": "Gorilla",
+            "health": 150,
+            "attack": 150,
+            "defence": 120,
+            "speed": 130,
+            "description": "blabla",
+            "imageURL": "Content/img/dolphin.png"
         }
     ]
     return {animals:animals};
